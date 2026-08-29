43ddcb9ea52d6165170e0644557cd17a51a3c21c
eytcar-creator <eyt.car@gmail.com>
2026-08-29 11:20:56 +0330

    feat: add management dashboard snapshot

---
+++ src/dashboard_service.py
@@ -0,0 +1,18 @@
+"""E.Y.T Management Dashboard service v1."""
+
+from decimal import Decimal
+
+def build_management_snapshot(invoices, stocks, agencies):
+    sales = sum((i.total for i in invoices), Decimal("0"))
+    receivables = sum((i.balance for i in invoices), Decimal("0"))
+    paid = sum((i.paid_amount for i in invoices), Decimal("0"))
+    low_stock = [s.sku for s in stocks if s.needs_reorder]
+    agency_scores = {a.code: a.score for a in agencies}
+
+    return {
+        "sales": sales,
+        "paid": paid,
+        "receivables": receivables,
+        "low_stock_skus": low_stock,
+        "agency_scores": agency_scores,
+    }
