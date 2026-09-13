4dc714a2f3bdff120140cfa69c4eb46acd8f56e0
eytcar-creator <eyt.car@gmail.com>
2026-09-13 11:28:27 +0330

    feat(api): expose cash collection controls

--- api/production/main.py
+++ api/production/main.py
@@ -24,6 +24,7 @@ from .payable_api import router as payable_router
 from .customer_portal_api import router as customer_portal_router
 from .customer_market_api import router as customer_market_router
 from .repeat_purchase_api import router as repeat_purchase_router
+from .cash_collection_api import router as cash_collection_router
 from ..orders.fastapi_router import router as order_router, configure_order_center
 from ..orders.order_center import OrderCenter
 from ..orders.postgres_adapter import PostgresOrderRepository, PostgresInventoryGateway
@@ -49,6 +50,7 @@ app.include_router(order_router)
 app.include_router(customer_portal_router)
 app.include_router(customer_market_router)
 app.include_router(repeat_purchase_router)
+app.include_router(cash_collection_router)
 
 def _configure_order_center() -> None:
     database_url = os.environ.get("DATABASE_URL")
