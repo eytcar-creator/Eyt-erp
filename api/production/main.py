eba9f84532c1795d3082e5904d7b6f21da6eb308
eytcar-creator <eyt.car@gmail.com>
2026-09-13 11:30:42 +0330

    feat(api): register Profit First dashboard router

--- api/production/main.py
+++ api/production/main.py
@@ -25,6 +25,7 @@ from .customer_portal_api import router as customer_portal_router
 from .customer_market_api import router as customer_market_router
 from .repeat_purchase_api import router as repeat_purchase_router
 from .cash_collection_api import router as cash_collection_router
+from .profit_dashboard_api import router as profit_dashboard_router
 from ..orders.fastapi_router import router as order_router, configure_order_center
 from ..orders.order_center import OrderCenter
 from ..orders.postgres_adapter import PostgresOrderRepository, PostgresInventoryGateway
@@ -51,6 +52,7 @@ app.include_router(customer_portal_router)
 app.include_router(customer_market_router)
 app.include_router(repeat_purchase_router)
 app.include_router(cash_collection_router)
+app.include_router(profit_dashboard_router)
 
 def _configure_order_center() -> None:
     database_url = os.environ.get("DATABASE_URL")
