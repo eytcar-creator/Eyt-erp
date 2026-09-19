031a9a952c150bcd9579aa0daae597b98bb46dbf
eytcar-creator <eyt.car@gmail.com>
2026-09-19 19:47:55 +0330

    feat: expose actual product and customer profitability

--- migrations/027_eyt_profit_actual_cost_link_v1.sql
+++ migrations/027_eyt_profit_actual_cost_link_v1.sql
@@ -47,6 +47,32 @@ BEGIN
 END;
 $$;
 
+CREATE OR REPLACE VIEW product_profitability_actual AS
+SELECT
+  i.product_id,
+  COUNT(DISTINCT i.order_no) AS order_count,
+  COALESCE(SUM(i.quantity),0) AS units_sold,
+  COALESCE(SUM(i.quantity*i.unit_price),0) AS sales,
+  COALESCE(SUM(i.quantity*COALESCE(i.actual_cost_snapshot,i.cost_snapshot,0)),0) AS cogs,
+  COALESCE(SUM(i.quantity*(i.unit_price-COALESCE(i.actual_cost_snapshot,i.cost_snapshot,0))),0) AS contribution_profit,
+  CASE WHEN COALESCE(SUM(i.quantity*i.unit_price),0)>0 THEN
+    SUM(i.quantity*(i.unit_price-COALESCE(i.actual_cost_snapshot,i.cost_snapshot,0)))
+    / SUM(i.quantity*i.unit_price)
+  ELSE 0 END AS contribution_margin
+FROM sales_order_items i
+GROUP BY i.product_id;
+
+CREATE OR REPLACE VIEW customer_profitability_actual AS
+SELECT
+  o.customer_id,
+  COUNT(*) AS order_count,
+  COALESCE(SUM(o.sales),0) AS net_sales,
+  COALESCE(SUM(o.contribution_profit),0) AS contribution_profit,
+  CASE WHEN COALESCE(SUM(o.sales),0)>0
+       THEN SUM(o.contribution_profit)/SUM(o.sales) ELSE 0 END AS contribution_margin
+FROM eyt_order_actual_profitability o
+GROUP BY o.customer_id;
+
 CREATE OR REPLACE VIEW eyt_order_actual_profitability AS
 SELECT
   o.order_no,
