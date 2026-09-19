d1a268d9f29ffd9856452bf44878d7b631d4d293
eytcar-creator <eyt.car@gmail.com>
2026-09-19 19:43:17 +0330

    feat: register production and material movement routers

--- api/production/fastapi_app.py
+++ api/production/fastapi_app.py
@@ -8,7 +8,11 @@ if FastAPI is not None:
     app = FastAPI(title="E.Y.T Production API", version="1.0.0")
     from .inventory_api import router as inventory_router
     from .core_master_api import router as core_master_router
+    from .production_control_api import router as production_control_router
+    from .inventory_production_api import router as inventory_production_router
     app.include_router(inventory_router)
     app.include_router(core_master_router)
+    app.include_router(production_control_router)
+    app.include_router(inventory_production_router)
 else:
     app = None
