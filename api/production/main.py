512f87e59b91436eedbbe7e4ad7bcbbcd206ff85
eytcar-creator <eyt.car@gmail.com>
2026-09-23 23:29:17 +0330

    feat: operationalize daily performance control (#64)
    * feat: add daily performance planning and control schema
    
    * feat: add daily performance control API
    
    * fix: keep daily production output independent from plan rows
    
    * feat: expose daily performance control API

--- api/production/main.py
+++ api/production/main.py
@@ -27,6 +27,7 @@ from .repeat_purchase_api import router as repeat_purchase_router
 from .cash_collection_api import router as cash_collection_router
 from .profit_dashboard_api import router as profit_dashboard_router
 from .performance_output_api import router as performance_output_router
+from .performance_control_api import router as performance_control_router
 from .strategy_action_api import router as strategy_action_router
 from .strategy_users_api import router as strategy_users_router
 from .strategy_notification_api import router as strategy_notification_router
@@ -67,6 +68,7 @@ app.include_router(repeat_purchase_router)
 app.include_router(cash_collection_router)
 app.include_router(profit_dashboard_router)
 app.include_router(performance_output_router)
+app.include_router(performance_control_router)
 app.include_router(strategy_action_router)
 app.include_router(strategy_users_router)
 app.include_router(strategy_notification_router)
